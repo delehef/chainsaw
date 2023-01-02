0e2de52f0df57505986dceeccd0e0be311811a67
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-01-02 12:38:30 +0100

    refactor: update the gene book

--- src/genebook.rs
+++ src/genebook.rs
@@ -3,26 +3,26 @@ use rusqlite::*;
 use std::collections::HashMap;
 use std::sync::Mutex;
 
+#[allow(dead_code)]
+pub enum GeneBook {
+    InMemory(HashMap<String, Gene>),
+    Cached(HashMap<String, Gene>),
+    Inline(Mutex<Connection>),
+}
+
 #[derive(Clone, Default)]
 pub struct Gene {
     pub gene: String,
     pub species: String,
 }
 
-pub enum GeneBook {
-    Cached(HashMap<String, Gene>),
-    Inline(Mutex<Connection>),
-}
-
 impl GeneBook {
-    pub fn cached(filename: &str) -> Result<Self> {
-        println!("Parsing the database...");
-
-        let conn = Connection::open(filename)
-            .with_context(|| format!("while connecting to {}", filename))?;
-        let mut query = conn.prepare("SELECT gene, protein, species FROM genomes")?;
+    fn get_rows<P: rusqlite::Params>(
+        mut query: rusqlite::Statement,
+        params: P,
+    ) -> Result<HashMap<String, Gene>> {
         let genes = query
-            .query_map([], |r| {
+            .query_map(params, |r| {
                 std::result::Result::Ok((
                     r.get::<_, String>(0)?,
                     r.get::<_, String>(1)?,
@@ -31,7 +31,7 @@ impl GeneBook {
             })?
             .collect::<Result<Vec<_>, _>>()?;
 
-        let r = genes
+        Ok(genes
             .into_iter()
             .map(|g| {
                 (
@@ -42,11 +42,36 @@ impl GeneBook {
                     },
                 )
             })
-            .collect();
-        println!("Done.");
+            .collect())
+    }
+
+    pub fn in_memory(filename: &str) -> Result<Self> {
+        let conn = Connection::open(filename)
+            .with_context(|| format!("while connecting to {}", filename))?;
+        let query = conn.prepare("SELECT gene, protein, species FROM genomes")?;
+        let r = Self::get_rows(query, [])?;
+        Ok(GeneBook::InMemory(r))
+    }
+
+    #[allow(dead_code)]
+    pub fn cached<S: AsRef<str>>(filename: &str, ids: &[S]) -> Result<Self> {
+        let conn = Connection::open(filename)
+            .with_context(|| format!("while connecting to {}", filename))?;
+        let query = conn.prepare(&format!(
+            "SELECT gene, protein, species FROM genomes WHERE protein IN ({})",
+            std::iter::repeat("?")
+                .take(ids.len())
+                .collect::<Vec<_>>()
+                .join(", ")
+        ))?;
+        let r = Self::get_rows(
+            query,
+            rusqlite::params_from_iter(ids.iter().map(|s| s.as_ref())),
+        )?;
         Ok(GeneBook::Cached(r))
     }
 
+    #[allow(dead_code)]
     pub fn inline(filename: &str) -> Result<Self> {
         let conn = Connection::open(filename)
             .with_context(|| format!("while connecting to {}", filename))?;
@@ -55,19 +80,19 @@ impl GeneBook {
 
     pub fn get(&self, g: &str) -> Result<Gene> {
         match self {
-            GeneBook::Cached(book) => book
-                .get(g)
-                .map(|g| g.clone())
-                .ok_or(anyhow::anyhow!("key not found")),
+            GeneBook::InMemory(book) | GeneBook::Cached(book) => {
+                book.get(g).cloned().ok_or_else(|| anyhow!("key not found"))
+            }
             GeneBook::Inline(conn_mutex) => {
                 let conn = conn_mutex.lock().expect("MUTEX POISONING");
-                let mut query = conn.prepare("SELECT gene, species FROM genomes WHERE protein=?")?;
+                let mut query =
+                    conn.prepare("SELECT gene, protein, species FROM genomes WHERE protein=?")?;
                 query
                     .query_row(&[g], |r| {
-                        rusqlite::Result::Ok(Gene {
-                            gene: r.get::<_, String>(0)?,
-                            species: r.get::<_, String>(1)?,
-                        })
+                        let gene = r.get::<_, String>(0)?;
+                        let species = r.get::<_, String>(5)?;
+
+                        rusqlite::Result::Ok(Gene { gene, species })
                     })
                     .with_context(|| "while accessing DB")
             }
